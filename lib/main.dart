42d5105b55b14b105bf234b14a53bf17c6a605c5
Erenalp <erenalpkesici@outlook.com>
2021-10-19 22:45:59 +0300

    vtest

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter_archive/flutter_archive.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:http/http.dart' as http;
-
+//MAIN
 const String XL_URL = "https://github.com/ErenalpKesici/Ders-Hatirlatici-Mobil/releases/download/Attachments/xl.zip";
 const String UPDATE_URL = "https://github.com/ErenalpKesici/Ders-Hatirlatici-Mobil/releases/download/Attachments/Update.txt";
 const String APK_URL = "https://github.com/ErenalpKesici/Ders-Hatirlatici-Mobil/releases/download/Attachments/app.apk";
