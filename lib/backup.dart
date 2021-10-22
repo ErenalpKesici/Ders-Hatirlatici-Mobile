15a84a001e99c5cf6bc832cea22743a9ae841df5
Erenalp <erenalpkesici@outlook.com>
2021-10-22 16:15:15 +0300

    initial cs

--- lib/backup.dart
+++ lib/backup.dart
@@ -1,5 +1,13 @@
 class Backup{
   String lecturer = "", course = "", topic = "", type = "", time = "", timeType = "";
+  Backup.initial(){
+    lecturer = "Tüm Eğiticiler";
+    course = "Tüm Sınıflar";
+    topic = "Tüm Konular";
+    type = "Tüm Tipler";
+    time = "10";
+    timeType = "Dakika";
+  }
   Backup(this.lecturer, this.course, this.topic, this.type, this.time, this.timeType);
   Backup.fromJson(Map<String, dynamic> json)
     : lecturer = json['lecturer'],
